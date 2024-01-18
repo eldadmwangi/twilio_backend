401a8f85f421913d2e829ca4d385fad4b975f503
Eldad <eldad.mwangi@paygoenergy.co>
2024-01-18 14:51:25 +0300

    fix-up

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY . .
 
 RUN npm install
 
-EXPOSE 3000
+EXPOSE 8080
 
 CMD ["node", "index.js"]
 
--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.post('/send-text',  async (req, res) => {
         }
     });
     
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
+    app.listen(8080, () => {
+      console.log('Server is running on port 8080');
     });
